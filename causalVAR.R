bb5e7ffe8dc0ded2e9ac6c02dbe0b492b2cd05e3
Kingfish Threesevens <kingfish777@users.noreply.github.com>
2017-06-22 13:31:53 -0500

    Update causalVAR.R

--- causalVAR.R
+++ causalVAR.R
@@ -3,6 +3,7 @@ library(vars)
 library(rcausal)
 library(plm)
 library(tidyverse)
+# note that you can also include "structured data" - these can be age range, gender, ethnicity, which allele an individual carries
 dat <- as.data.frame(read_tsv("/Users/smalec/Downloads/fake_panel_data.csv"))
 pdat <- pdata.frame(dat, index = c("visit", "patient"))
 
