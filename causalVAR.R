be5cd6b84ab76c2786b28861ea0f6facdb3df9de
Kingfish Threesevens <kingfish777@users.noreply.github.com>
2017-06-22 13:33:19 -0500

    Update causalVAR.R

--- causalVAR.R
+++ causalVAR.R
@@ -6,6 +6,5 @@ library(tidyverse)
 # note that you can also include "structured data" - these can be age range, gender, ethnicity, which allele an individual carries
 dat <- as.data.frame(read_tsv("/Users/smalec/Downloads/fake_panel_data.csv"))
 pdat <- pdata.frame(dat, index = c("visit", "patient"))
-
 tetrad.input <- residuals(VAR(pdat[,3:7]))
-pcmax(tetrad.input)
+tetrad.output <- pcmax(tetrad.input, continuous = TRUE)
