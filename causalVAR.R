6e6e6263e6c59d286265002f3611025281955218
Kingfish Threesevens <kingfish777@users.noreply.github.com>
2018-08-20 13:48:37 -0400

    Update causalVAR.R

--- causalVAR.R
+++ causalVAR.R
@@ -4,7 +4,18 @@ library(rcausal)
 library(plm)
 library(tidyverse)
 # note that you can also include "structured data" - these can be age range, gender, ethnicity, which allele an individual carries
-dat <- as.data.frame(read_tsv("paneldata.csv"))
+
+# read tab delimited data into data frame
+dat <- as.data.frame(read_tsv("paneldata.tsv"))
+
+# transform datframe into paneldata object
 pdat <- pdata.frame(dat, index = c("visit", "patient"))
+
+# extract the residuals (error) from the SVAR model of the paneldata (pdat) object)
 tetrad.input <- residuals(VAR(pdat[,3:7]))
-tetrad.output <- rcausal::pc(tetrad.input, continuous = TRUE)
+
+tetrad.output <- rcausal::pc(tetrad.input)
+#print results
+print(tetrad.output)
+
+
